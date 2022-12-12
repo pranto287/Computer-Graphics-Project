729b64eb352f87564e34db30c89f3200ca9a2974
MOSTOFA KAMAL <65121330+pranto287@users.noreply.github.com>
2022-12-12 18:41:26 +0600

    Commit 445

---
+++ Arab.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include<GL/gl.h>
+#include <GL/glut.h>
+#include<cmath>
+
+
+void myDisplay(void)
+{
+
+glFlush ();
+}
+
+void myInit (void)
+{
+glClearColor(.3, .4, 1, 1);
+glMatrixMode(GL_PROJECTION);
+glLoadIdentity();
+gluOrtho2D(0.0, 840.0, 0.0, 480.0);
+}
+
+
+
+
+
+int main(int argc, char** argv)
+{
+glutInit(&argc, argv);
+glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+glutInitWindowSize (840, 480);
+glutInitWindowPosition (200, 200);
+glutCreateWindow ("");
+glutDisplayFunc(myDisplay);
+myInit ();
+glutMainLoop();
+}
+
